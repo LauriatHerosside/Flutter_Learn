f3de0528dd28a3b45b21006a492e7e17ac7a464e
LauriatHerosside <laulitadeo@gmail.com>
2022-08-04 18:04:07 +0000

    SignIn

--- lib/screens/sign_in/components/body_sign_in.dart
+++ lib/screens/sign_in/components/body_sign_in.dart
@@ -1,10 +1,14 @@
-import 'package:e_commerce/components/default_button.dart';
-import 'package:e_commerce/constant.dart';
-import 'package:e_commerce/size_config.dart';
+import 'package:e_commerce/screens/sign_in/components/sign_form.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
+import 'package:flutter_svg/parser.dart';
 
 import '../../../components/custom_surfix_icon.dart';
+import '../../../components/default_button.dart';
+import '../../../components/form_error.dart';
+import '../../../components/social_card.dart';
+import '../../../constant.dart';
+import '../../../size_config.dart';
 
 class BodySignIn extends StatelessWidget {
   const BodySignIn({Key? key}) : super(key: key);
@@ -14,72 +18,69 @@ class BodySignIn extends StatelessWidget {
     return SafeArea(
       child: SizedBox(
         width: double.infinity,
-        child: Column(
-          children: [
-            Text(
-              "Bienvenue Chez O'Miel",
-              style: TextStyle(
-                  fontSize: getProportionateScreenWidth(28),
-                  fontWeight: FontWeight.bold,
-                  color: Colors.black),
-            ),
-            Text(
-              "Connection avec email & le mot de passe \n ou  continuer avec un reseau social",
-              textAlign: TextAlign.center,
-            ),
-            SignInForm()
-          ],
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              SizedBox(
+                height: SizeConfig.screenHeight! * 0.08,
+              ),
+              Text(
+                "Bienvenue Chez O'Miel",
+                style: TextStyle(
+                    fontSize: getProportionateScreenWidth(28),
+                    fontWeight: FontWeight.bold,
+                    color: Colors.black),
+              ),
+              Text(
+                "Connection avec email & le mot de passe \n ou  continuer avec un reseau social",
+                textAlign: TextAlign.center,
+              ),
+              SizedBox(
+                height: SizeConfig.screenHeight! * 0.08,
+              ),
+              SignForm(),
+              SizedBox(
+                height: SizeConfig.screenHeight! * 0.08,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  SocialCard(
+                    icon: "assets/icons/google-icon.svg",
+                    press: () {},
+                  ),
+                  SocialCard(
+                    icon: "assets/icons/facebook-2.svg",
+                    press: () {},
+                  ),
+                  SocialCard(
+                    icon: "assets/icons/twitter.svg",
+                    press: () {},
+                  )
+                ],
+              ),
+              SizedBox(
+                height: getProportionateScreenHeight(28),
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    "Vous n'avez pas de compte ? ",
+                    style: TextStyle(fontSize: getProportionateScreenWidth(16)),
+                  ),
+                  Text(
+                    "Créer  ",
+                    style: TextStyle(
+                        fontSize: getProportionateScreenWidth(16),
+                        color: kPrimaryColor),
+                  )
+                ],
+              )
+            ],
+          ),
         ),
       ),
     );
-  }
-}
-
-class SignInForm extends StatelessWidget {
-  const SignInForm({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Form(
-      child: Padding(
-        padding:
-            EdgeInsets.symmetric(horizontal: getProportionateScreenWidth(20)),
-        child: Column(children: [
-          buildEmailFormField(),
-          SizedBox(
-            height: getProportionateScreenWidth(20),
-          ),
-          buildPasswordFormField(),
-          SizedBox(
-            height: getProportionateScreenWidth(20),
-          ),
-          DefaultButton(text: "Continue", press: () {})
-        ]),
-      ),
-    );
-  }
-
-  TextFormField buildPasswordFormField() {
-    return TextFormField(
-        obscureText: true,
-        decoration: InputDecoration(
-            labelText: "Mot de passe",
-            hintText: "Entrez votre mot de passe",
-            floatingLabelBehavior: FloatingLabelBehavior.always,
-            suffixIcon: CustomSurffixIcon(
-              svgIcon: "assets/icons/Lock.svg",
-            )));
-  }
-
-  TextFormField buildEmailFormField() {
-    return TextFormField(
-        keyboardType: TextInputType.emailAddress,
-        decoration: InputDecoration(
-            labelText: "Email",
-            hintText: "Entrez votre email",
-            floatingLabelBehavior: FloatingLabelBehavior.always,
-            suffixIcon: CustomSurffixIcon(
-              svgIcon: "assets/icons/Mail.svg",
-            )));
   }
 }
---
+++ lib/screens/sign_in/components/sign_form.dart
@@ -0,0 +1,152 @@
+import 'package:flutter/material.dart';
+
+import '../../../components/custom_surfix_icon.dart';
+import '../../../components/default_button.dart';
+import '../../../components/form_error.dart';
+import '../../../constant.dart';
+import '../../../size_config.dart';
+
+class SignForm extends StatefulWidget {
+  const SignForm({Key? key}) : super(key: key);
+
+  @override
+  _SignFormState createState() => _SignFormState();
+}
+
+class _SignFormState extends State<SignForm> {
+  final _formKey = GlobalKey<FormState>();
+  late String email;
+  late String password;
+  bool remember = false;
+  final List<String> errors = [];
+  @override
+  Widget build(BuildContext context) {
+    return Form(
+      key: _formKey,
+      child: Padding(
+        padding:
+            EdgeInsets.symmetric(horizontal: getProportionateScreenWidth(20)),
+        child: Column(children: [
+          buildEmailFormField(),
+          SizedBox(
+            height: getProportionateScreenWidth(20),
+          ),
+          buildPasswordFormField(),
+          SizedBox(
+            height: getProportionateScreenWidth(20),
+          ),
+          SizedBox(
+            height: getProportionateScreenWidth(5),
+          ),
+          Row(
+            children: [
+              Checkbox(
+                value: remember,
+                activeColor: kPrimaryColor,
+                onChanged: (value) {
+                  setState(() {
+                    remember = value!;
+                  });
+                },
+              ),
+              Text("Se souvenir"),
+              Spacer(),
+              Text(
+                "Mot de passe oublier",
+                style: TextStyle(decoration: TextDecoration.underline),
+              )
+            ],
+          ),
+          FormError(errors: errors),
+          SizedBox(
+            height: getProportionateScreenHeight(10),
+          ),
+          DefaultButton(
+              text: "Continue",
+              press: () {
+                if (_formKey.currentState!.validate()) {
+                  _formKey.currentState!.save();
+                }
+                ;
+              })
+        ]),
+      ),
+    );
+  }
+
+  TextFormField buildPasswordFormField() {
+    return TextFormField(
+        obscureText: true,
+        onSaved: ((newValue) => password = newValue!),
+        onChanged: (value) {
+          if (value.isNotEmpty && errors.contains(kPassNullError)) {
+            setState(() {
+              errors.remove(kPassNullError);
+            });
+          } else if (value.length >= 8 && errors.contains(kShortPassError)) {
+            setState(() {
+              errors.remove(kShortPassError);
+            });
+          }
+          return null;
+        },
+        validator: (value) {
+          if (value!.isEmpty && !errors.contains(kPassNullError)) {
+            setState(() {
+              errors.add(kPassNullError);
+            });
+          } else if (value.length < 8 && !errors.contains(kShortPassError)) {
+            setState(() {
+              errors.add(kShortPassError);
+            });
+          }
+          return null;
+        },
+        decoration: InputDecoration(
+            labelText: "Mot de passe",
+            hintText: "Entrez votre mot de passe",
+            floatingLabelBehavior: FloatingLabelBehavior.always,
+            suffixIcon: CustomSurffixIcon(
+              svgIcon: "assets/icons/Lock.svg",
+            )));
+  }
+
+  TextFormField buildEmailFormField() {
+    return TextFormField(
+        keyboardType: TextInputType.emailAddress,
+        onSaved: ((newValue) => email = newValue!),
+        onChanged: (value) {
+          if (value.isNotEmpty && errors.contains(kEmailNullError)) {
+            setState(() {
+              errors.remove(kEmailNullError);
+            });
+          } else if (emailValidatorRegExp.hasMatch(value) &&
+              errors.contains(kInvalidEmailError)) {
+            setState(() {
+              errors.remove(kInvalidEmailError);
+            });
+          }
+          return null;
+        },
+        validator: (value) {
+          if (value!.isEmpty && errors.contains(kEmailNullError)) {
+            setState(() {
+              errors.add(kEmailNullError);
+            });
+          } else if (!emailValidatorRegExp.hasMatch(value) &&
+              !errors.contains(kInvalidEmailError)) {
+            setState(() {
+              errors.add(kInvalidEmailError);
+            });
+          }
+          return null;
+        },
+        decoration: InputDecoration(
+            labelText: "Email",
+            hintText: "Entrez votre email",
+            floatingLabelBehavior: FloatingLabelBehavior.always,
+            suffixIcon: CustomSurffixIcon(
+              svgIcon: "assets/icons/Mail.svg",
+            )));
+  }
+}
